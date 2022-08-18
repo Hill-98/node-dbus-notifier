3a45b1f5f32fa084cdd68001aa24b59447937428
Zhong Lufan <lufanzhong@gmail.com>
2022-08-18 23:45:58 +0800

    Add inline doc

--- index.d.ts
+++ index.d.ts
@@ -11,63 +11,118 @@ export enum Status {
 
 // eslint-disable-next-line no-shadow
 export enum Reason {
+  /** The notification expired. */
   expired = 1,
+  /** The notification was dismissed by the user. */
   closeByUser = 2,
+  /** The notification was closed by a call to `CloseNotification`. */
   closeByCall = 3,
+  /** Undefined/reserved reasons. */
   undefined = 4,
 }
 
 export interface Hints {
   actionIcons: boolean;
+  /** The type of notification this is. */
   category: string;
+  /** This specifies the name of the desktop filename representing the calling program. This should be the same as the prefix used for the application's .desktop file. */
   desktopEntry: string;
+  /** This is a raw data image format which describes the width, height, rowstride, has alpha, bits per sample, channels and image data respectively. */
+  imageData: never;
+  /**
+   * Alternative way to define the notification image.
+   * @see {@link https://specifications.freedesktop.org/notification-spec/1.2/ar01s05.html Icons and Images}
+   */
   imagePath: string;
+  /**
+   * When set the server will not automatically remove the notification when an action has been invoked.
+   * The notification will remain resident in the server until it is explicitly removed by the user or by the sender.
+   * This hint is likely only useful when the server has the "persistence" capability.
+   */
   resident: boolean;
+  /** The path to a sound file to play when the notification pops up. */
   soundFile: string;
+  /**
+   * A themeable named sound from the freedesktop.org sound naming specification to play when the notification pops up.
+   * @see {@link http://0pointer.de/public/sound-naming-spec.html Sound Naming Specification}
+   */
   soundName: string;
+  /** Causes the server to suppress playing any sounds, if it has that ability. */
   suppressSound: boolean;
+  /** When set the server will treat the notification as transient and by-pass the server's persistence capability, if it should exist. */
   transient: boolean;
+  /** Specifies the X location on the screen that the notification should point to. The "y" hint must also be specified. */
   x: number;
+  /** Specifies the Y location on the screen that the notification should point to. The "x" hint must also be specified. */
   y: number;
+  /** The urgency level. */
   urgency: number;
+  /** Some notification servers support it to show progress (non-standard specification) */
+  value: number;
 }
 
 export interface NotifyConfig {
+  /** The optional name of the application sending the notification. */
   appName: string;
-  replacesId: number;
+  /**
+   * The optional program icon of the calling application.
+   * @see {@link https://specifications.freedesktop.org/notification-spec/1.2/ar01s05.html Icons and Images}
+   */
   appIcon: string;
+  /** The optional notification ID that this notification replaces. */
+  replacesId: number;
+  /** The summary text briefly describing the notification. */
   summary: string;
+  /** The optional detailed body text. */
   body: string;
+  /**
+   * Optional hints that can be passed to the server from the client program.
+   * @see {@link https://specifications.freedesktop.org/notification-spec/1.2/ar01s08.html Hints}
+   */
   hints: Partial<Hints>;
+  /** The timeout time in milliseconds since the display of the notification at which the notification should automatically close. */
   timeout: number;
 }
 
 export interface NotificationClosedResult {
+  /** The ID of the notification that was closed. */
   id: number;
+  /** The reason the notification was closed. */
   reason: Reason;
 }
 
 export class Notify extends EventEmitter {
+  /** The notification ID. */
   readonly id: number;
 
+  /** The notification status. */
   readonly status: Status;
 
+  /** Initialize a notification */
   constructor(config: Partial<NotifyConfig>);
 
+  /** Add an action to the notification with random action key, return action key. */
   addAction(text: string, callback: () => void): string;
 
+  /** Add an action to notification with custom action key, return action key. */
   addAction(text: string, key: string, callback: () => void): string;
 
+  /** Add inline reply input box to the notification, the first parameter of the callback function is the submitted message, return action key. */
   addAction(text: string, key: 'inline-reply', callback: (message: string) => void): string;
 
+  /** Use `org.freedesktop.Notifications.CloseNotification` actively close notification. */
   close(): Promise<void>;
 
+  /** Use action key remove an added action */
   removeAction(key: string): boolean;
 
+  /** Alias for `removeAction("default")`. */
   removeDefaultAction(): boolean;
 
+  /** Alias for `addAction("", "default", callback)`. */
   setDefaultAction(callback: () => void): void;
 
+  /** Use `org.freedesktop.Notifications.Notify` to display the notification and mark the Promise as resolved when the notification is dismissed. */
   show(): Promise<NotificationClosedResult>;
 
   addListener(event: 'close', listener: (result: NotificationClosedResult) => void): this;
@@ -98,21 +153,28 @@ export class Notify extends EventEmitter {
 
   removeListener(event: 'show', listener: (id: number) => void): this;
 
+  /** The capabilities supported by the notification server. */
   static supportedCapabilities(): Promise<string[]>;
 }
 
 export interface ConfigInterface {
+  /** If set to `false`, the internal DBus session will not be automatically disconnected. */
   autoDisconnectSessionBus: boolean;
 }
 
 export const Config: ConfigInterface;
 
+/** Disconnect the internal DBus session. */
 export function disconnectSessionBus(): void;
 
+/** Get DBus interface `org.freedesktop.Notifications`. */
 export function getInterface(): ClientInterface;
 
+/** Set DBus interface `org.freedesktop.Notifications`. */
 export function setInterface(notificationInterface?: ClientInterface): void;
 
+/** Get DBus Session. */
 export function getSessionBus(): MessageBus;
 
+/** Set DBus session. */
 export function setSessionBus(sessionBus?: MessageBus): void;
